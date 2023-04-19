0599a67271b9fc71607a8b96a2c9ab3aa6683cf6
Jorge Silva <jorgesilva.ti@hotmail.com>
2023-04-19 16:46:37 -0300

    create

---
+++ docs/Lyceum/Api/pre/CEE3DCBFFD8276837B4F46018BE75855/AutenticarCarteirinha/04303408758244/info.js
@@ -0,0 +1,10 @@
+const matricula = "N522BE4"
+const nome = "STEFANYE ALVES PEREIRA"
+const rg = "2830397 SSP/DF"
+const dt_nascimento = "27/05/1995"
+const curso_id = "25201"
+const curso = "ENFERMAGEM"
+const validade = "JUL/2023"
+
+const unidade = "0130 - BRASILIA"
+const campus = "BRASILIA"
