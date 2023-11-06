38ac9deeb5ea531e8bbcd1c59c6268b491a841f9
Jorge Silva da Costa <jorge.costa@comunix.net.br>
2023-11-06 09:10:42 -0300

    Add new user

---
+++ docs/Lyceum/Api/pre/CEE3DCBFFD8276837B4F46018BE75855/AutenticarCarteirinha/87683498758253/info.js
@@ -0,0 +1,8 @@
+const matricula = "N448H02"
+const nome = "LUIS FERNANDO MOREIRA DE PAIVA"
+const rg = "2576511 SSP/DF"
+const dt_nascimento = "30/11/1992"
+const curso_id = "32015"
+const curso = "Comunicação Social"
+const unidade = "0130 - BRASILIA"
+const campus = "BRASILIA"
