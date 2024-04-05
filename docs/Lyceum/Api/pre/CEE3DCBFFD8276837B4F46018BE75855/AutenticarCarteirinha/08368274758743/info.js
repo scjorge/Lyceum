03ffeda287b2bf0ffef4b1485d81dbc409dff0d5
Jorge Silva <jorgesilva.ti@hotmail.com>
2024-04-04 22:00:20 -0300

    Add new user

---
+++ docs/Lyceum/Api/pre/CEE3DCBFFD8276837B4F46018BE75855/AutenticarCarteirinha/08368274758743/info.js
@@ -0,0 +1,8 @@
+const matricula = "N367H02"
+const nome = "LANNA KEURY SOUSA LIMA"
+const rg = "4.050.986 SSP/DF"
+const dt_nascimento = "20/02/2005"
+const curso_id = "38620"
+const curso = "CIÊNCIA DA COMPUTAÇÃO"
+const unidade = "0130 - BRASILIA"
+const campus = "BRASILIA"
