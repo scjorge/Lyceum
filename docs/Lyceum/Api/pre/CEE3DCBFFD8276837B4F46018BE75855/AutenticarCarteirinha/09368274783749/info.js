76cdfac526278034c5b8192568f8f1e4054590cb
Jorge Silva da Costa <jorge.costa@comunix.net.br>
2024-05-03 13:18:06 -0300

    Add new user

---
+++ docs/Lyceum/Api/pre/CEE3DCBFFD8276837B4F46018BE75855/AutenticarCarteirinha/09368274783749/info.js
@@ -0,0 +1,8 @@
+const matricula = "N287H07"
+const nome = "JOÃO VITOR AMARAL E ROZÁRIO"
+const rg = "5425335 SSP/GO"
+const dt_nascimento = "17/03/1999"
+const curso_id = "38620"
+const curso = "ENGENHARIA CIVIL"
+const unidade = "0130 - BRASILIA"
+const campus = "BRASILIA"
