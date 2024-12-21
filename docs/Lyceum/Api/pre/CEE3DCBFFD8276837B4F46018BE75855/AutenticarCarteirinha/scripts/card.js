b4fdb438a27423df16b6f58f861641d8098f7423
Jorge Silva <jorgesilva.ti@hotmail.com>
2024-12-21 08:59:37 -0300

    change date format

--- docs/Lyceum/Api/pre/CEE3DCBFFD8276837B4F46018BE75855/AutenticarCarteirinha/scripts/card.js
+++ docs/Lyceum/Api/pre/CEE3DCBFFD8276837B4F46018BE75855/AutenticarCarteirinha/scripts/card.js
@@ -1,4 +1,6 @@
 function transformarFormatoData(data) {
+    // change data from 01/01/1999 to 01/01/99
+
     var partesData = data.split('/');
     var anoAbreviado = partesData[2].substring(2);
     var novaData = partesData[0] + '/' + partesData[1] + '/' + anoAbreviado;
@@ -9,6 +11,6 @@ document.getElementById("curso").innerHTML = curso.toUpperCase();
 document.getElementById("nome").innerHTML = "<b>NOME: </b>" + nome.toUpperCase();
 document.getElementById("matricula").innerHTML = "<b>MATRICULA: </b>" + matricula.toUpperCase();
 document.getElementById("rg").innerHTML = "<b>IDENTIDADE: </b>" + rg;
-document.getElementById("dt_nascimento").innerHTML = "<b>DATA NASCIMENTO: </b>" + transformarFormatoData(dt_nascimento);
+document.getElementById("dt_nascimento").innerHTML = "<b>DATA NASCIMENTO: </b>" + dt_nascimento;
 document.getElementById("campus").innerHTML = "<b>CAMPUS: </b>" + campus.toUpperCase();
 document.getElementById("validade").innerHTML = "VALIDADE: " + getValidade();
